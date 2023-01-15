31eeaf9776fba710d8960194740339e6e8b2535f
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-15 17:24:13 +0530

    Updated.

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,21 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [ele, setFlag] = useState("");
+  
+  function change(){
+    //setFlag(true);
+    setFlag(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+  }
   return (
+    <>
     <div id="main">
       // Do not alter the main div
     </div>
+    {ele}
+    {/* <p id="para" className={flag ? "sho" : "hide"}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> */}
+    <button id="click" onClick={change}>show</button>
+    </>
   );
 }
 
